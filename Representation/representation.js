16a148963510b6d611ee89a37ed347a8d3d97f9d
hanum-lee <chococloudy@gmail.com>
2019-04-11 10:42:15 -0600

    Basic functionally done

--- Representation/representation.js
+++ Representation/representation.js
@@ -1,5 +1,7 @@
 window.onload = function(){
     setup();
+
+
 };
 const PADDING_FOR_LABELS = 150;
 
@@ -16,6 +18,7 @@ let margin = {top:20, left:80, bottom:30, right:20};
     //width = 960 - margin.left - margin.right,
     //height = 600 - margin.top - margin.bottom;
 
+let selectedStreamer = [];
 
 let Barchart = function (){
     this.data;
@@ -229,7 +232,7 @@ let Barchart = function (){
         let labels = ["Max Viewer","Ave Viewer"];
         for(let i = 0; i < 2; i ++){
             let temparray = [];
-            for(let j = 0; j < 3; j++){
+            for(let j = 0; j < tempdata.length; j++){
                 let tempjson = {
                   "Game" : tempdata[j]["Game"],
                   "Viewer" : tempdata[j][labels[i]]
@@ -243,26 +246,151 @@ let Barchart = function (){
 
     };
 
+    this.removeEverything = function () {
+        this.svgContainer.selectAll('.layer').remove();
+        this.svgContainer.selectAll('g').remove();
+    };
+
+
+
+
+};
+
+let scattrplot = function(){
+    this.data;
+    this.width = 1000;
+    this.height = 600;
+
+    this.svg_Container;
+    this.datapoints;
+
+    this.xAxis;
+    this.yAxis;
+
+    this.xAxisScale;
+    this.yAxisScale;
+
+    this.setupScales = function(xRange, xDomain, yRange, yDomain){
+        this.xAxisScale = d3.scaleLinear()
+            .domain(xDomain)
+            .range(xRange);
+
+        this.yAxisScale = d3.scaleLinear()
+            .domain(yDomain)
+            .range(yRange);
+    };
+
+    this.setupAxes = function(xLabel, yLabel){
+        xLabel = xLabel === undefined ? "Life Satisfaction" : xLabel;
+        yLabel = yLabel === undefined ? "Employment Rate" : yLabel;
+
+        // call d3's axisBottom for the x-axis
+        this.xAxis = d3.axisBottom(this.xAxisScale)
+        // .tickSize(-this.height + MARGINS.bottom + MARGINS.top)
+            .ticks(10)
+            .tickPadding(10);
+        // call d3's axisLeft for the y-axis
+        this.yAxis = d3.axisLeft(this.yAxisScale)
+        // .tickSize(-this.width + MARGINS.left*2)
+            .ticks(10)
+            .tickPadding(10);
+
+        // call our axes inside "group" (<g></g>) objects inside our SVG container
+        this.svgContainer.append("g")
+            .attr("transform", `translate(0, ${this.height - margin.bottom })`)
+            .call(this.xAxis);
+        this.svgContainer.append("g")
+            .attr("transform", `translate(${margin.left}, 0)`)
+            .call(this.yAxis);
+
+        // add text labels
+        this.svgContainer.append("text")
+            .attr("x", margin.left)
+            .attr("y", (this.height)/2)
+            .attr("transform", `rotate(-90, ${margin.left / 3}, ${this.height/2})`)
+            .style("text-anchor", "middle")
+            .text(yLabel);
+        this.svgContainer.append("text")
+            .attr("x", (this.width)/2)
+            .attr("y", (this.height - margin.top))
+            .style("text-anchor", "middle")
+            .text(xLabel);
+
+    };
+
+    /**
+     * Function createCircles initializes the datapoints in our scatterplot
+     * @param xAxisSelector the data property for values to appear in x-axis
+     * @param yAxisSelector the data property for values to appear in y-axis
+     */
+    this.createCircles = function(xAxisSelector, yAxisSelector){
+        // default to Life Satisfaction and Employment Rate
+        xAxisSelector = xAxisSelector === undefined ? "Life satisfaction" : xAxisSelector;
+        yAxisSelector = yAxisSelector === undefined ? "Employment rate" : yAxisSelector;
+
+        var scatterplot = this;
+
+        // Use D3's selectAll function to create instances of SVG:circle virtually
+        // per item in our data array
+        this.datapoints = this.svgContainer.selectAll("circle")
+            .data(this.data)    // use the data we loaded from CSV
+            .enter()            // access the data item (e.g., this.data[0])
+            .append("circle")   // add the circle element into our SVG container
+            .attr("r", 8)       // change some of the attributes of our circles
+            // function(d){ return d; } -> allows us to access the data we entered
+            .attr("cx", function(d){
+                // use the D3 scales we created earlier to map our data values to pixels on screen
+                return scatterplot.xAxisScale(d[xAxisSelector]);
+            })
+            .attr("cy", function(d){
+                return scatterplot.yAxisScale(d[yAxisSelector]);
+            })
+            .on("mouseover", function(d) {
+
+            })
+            .on("mouseout", function(d) {
+
+            })
+            // change some styling
+            .style("fill", "coral")
+            .style("stroke", "white")
+            // add a text to show up on hover
+            .append("svg:title")
+            .text(function(d){
+                return d[LABELS[0]];
+            });
+    };
+
+    this.highlight = function (selectedData) {
+        console.log(selectedData);
+
+    };
 
 
 
 };
 let vis_barchart;
+let vis_scatterplot;
 
 function setup(){
     vis_barchart = new Barchart();
     vis_barchart.svgContainer = d3.select("#vis_individual");
     //vis_barchart.width = $("#" + _vis_scatterplot.svgContainer.attr("id")).width();
     //vis_barchart.height = $("#" + _vis_scatterplot.svgContainer.attr("id")).height();
-
+    vis_scatterplot = new scattrplot();
+    vis_scatterplot.svgContainer = d3.select('#vis_scatterplot');
     loadData("TwitchStreamerData.csv");
 }
 
 function loadData(path){
     d3.csv(path).then(function (data) {
 
+        let streamerlist;
+        data.forEach(function(entry){
+            $('#streamerlist').append('<li>' + entry[LABELS[0]] + '</li>');
+        });
 
-        let gameTitle = getGametitle(data);
+        let gameTitle = getGametitle([data[0],data[1]]);
         let viewerCount = getViewers(data);
         let gameData = [
             {"Game":gameTitle[0],"Viewer": viewerCount[0]},
@@ -270,34 +398,67 @@ function loadData(path){
             {"Game":gameTitle[2],"Viewer": viewerCount[2]},
         ];
 
-        let sortedData = getIndvData(data[0]);
-        vis_barchart.data = sortedData;
+        let sortedData = getIndvData([data[0],data[1]]);
+        //vis_barchart.data = sortedData;
         console.log(gameData);
 
-        vis_barchart.setupScales(gameTitle, [vis_barchart.height-margin.bottom, margin.top], [0, 50000]);
-        vis_barchart.setupAxes(gameTitle.length, "Viewer count");
-        vis_barchart.setLayerDataTest();
-        vis_barchart.setlayerData();
+
+        // vis_barchart.setupScales(gameTitle, [vis_barchart.height-margin.bottom, margin.top], [0, 50000]);
+        // vis_barchart.setupAxes(gameTitle.length, "Viewer count");
+        // vis_barchart.setLayerDataTest();
+        //vis_barchart.setlayerData();
         //vis_barchart.setLayers();
         //vis_barchart.createBars("Game","Ave Viewer");
-        vis_barchart.createBarsTest();
+        //vis_barchart.createBarsTest();
         //vis_barchart.createBarsMax("Game","Max Viewer");
+        vis_scatterplot.data = data;
+        vis_scatterplot.setupScales([margin.left, vis_scatterplot.width - margin.right],[0, 30000],
+        [margin.bottom, vis_scatterplot.height- margin.top],[16000,0]);
+        vis_scatterplot.setupAxes(LABELS[2],LABELS[1]);
+        vis_scatterplot.createCircles(LABELS[2],LABELS[1]);
+
+        $('#streamerlist li').click(function(){
+            console.log($(this).text());
+            let steamerid = $(this).text();
+            if(selectedStreamer.indexOf((steamerid)) === -1){
+                selectedStreamer.push(data[$(this).index()]);
+                $(this).addClass("selected");
+                console.log(data[$(this).index()]);
+
+            }else{
+
+                //selectedStreamer.push($(this).text());
+            }
+
+            vis_barchart.removeEverything();
+            sortedData= getIndvData(selectedStreamer);
+
+            gameTitle = getGametitle(sortedData);
+
+            vis_barchart.setupScales(gameTitle, [vis_barchart.height-margin.bottom, margin.top], [0, 50000]);
+            vis_barchart.setupAxes(gameTitle.length, "Viewer count");
+            vis_barchart.data =sortedData;
+
+            vis_barchart.setLayerDataTest();
+            vis_barchart.createBarsTest();
+            console.log(selectedStreamer);
+
+        });
+
     });
 }
 
 function getGametitle(data){
     let gameTitles = [];
-    let tempdata = data[0];
-    gameTitles.push(tempdata["Most streamed game title (Game Title 1)"]
-        ,tempdata["Game title which had most number of viewer (Game Title 2)"]
-        ,tempdata["Game title which has most number of average viewer (Game Title 3)"]);
-    // data.forEach(function (e) {
-    //     console.log("data:",e);
-    //     console.log(e["Most streamed game title (Game Title 1)"]);
-    //    gameTitles.push(e["Most streamed game title (Game Title 1)"]
-    //        ,e["Game title which had most number of viewer (Game Title 2)"]
-    //        ,e["Game title which has most number of average viewer (Game Title 3)"]);
-    // });
+    //let tempdata = data[0];
+    // gameTitles.push(tempdata["Most streamed game title (Game Title 1)"]
+    //     ,tempdata["Game title which had most number of viewer (Game Title 2)"]
+    //     ,tempdata["Game title which has most number of average viewer (Game Title 3)"]);
+    data.forEach(function (e) {
+        console.log("data:",e.Game);
+        //console.log(e["Most streamed game title (Game Title 1)"]);
+       gameTitles.push(e.Game);
+    });
     //console.log(gameTitles);
     return gameTitles
 }
@@ -315,7 +476,7 @@ function getViewers(data) {
 function getIndvData(data){
     let dataSorted = [];
     let offset = 4;
-
+    console.log(data);
     // for (let i = 1; i < 4; i++){
     //     let temp = [];
     //     for(let j = 0; j < 3;j++){
@@ -329,19 +490,26 @@ function getIndvData(data){
     //
     //     dataSorted.push(temp);
     // }
-    for (let i = 0; i <3 ; i++){
-        dataSorted.push(
-            {"Game":data[LABELS[3 + offset*i]],
-                "Ave Viewer":data[LABELS[3 + offset * i + 1]],
-                "Max Viewer":data[LABELS[3 + offset * i + 2]],
-                //"Streamed Time" : data[LABELS[3 + offset * i+3]]
-            }
-        );
-    }
+    data.forEach(function(e){
+       console.log(e);
+        for (let i = 0; i <3 ; i++){
+            let gameandid = e[LABELS[3 + offset*i]] +' ' + e[LABELS[0]];
+            dataSorted.push(
+                {"Game":gameandid,
+                    "Ave Viewer":e[LABELS[3 + offset * i + 1]],
+                    "Max Viewer":e[LABELS[3 + offset * i + 2]],
+                    //"SteamerID":e[LABELS[0]]
+                    //"Streamed Time" : data[LABELS[3 + offset * i+3]]
+                }
+            );
+        }
+
+    });
+
 
 
     console.log(dataSorted);
     return dataSorted;
-
-
 }
+
+
