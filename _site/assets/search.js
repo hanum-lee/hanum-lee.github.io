f7a8446fac915f643e544f9819e7e31f1ff878bd
hanum-lee <chococloudy@gmail.com>
2020-06-28 02:23:29 -0600

    CPSC581P0 post added

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"testing",'url':"/2020/06/26/testing.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"CPSC581 Project 0, Button",'url':"/2019/02/04/CPSC581P0.html"},{'title':"testing",'url':"/2020/06/26/testing.html"}]};
